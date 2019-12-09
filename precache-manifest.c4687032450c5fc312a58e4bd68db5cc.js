738d657c0761d2bbc90d453517125e930909e797
Sajantoor98 <Sajantoor98@gmail.com>
2019-12-08 21:14:23 -0800

    Updates

---
+++ precache-manifest.c4687032450c5fc312a58e4bd68db5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8226dd70cfd7011b6908fc1fe595aa42",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "887b6ad1d85c5ec103f7",
+    "url": "/calculator/static/css/main.207429a5.chunk.css"
+  },
+  {
+    "revision": "ff3015cee1a2ec8d9913",
+    "url": "/calculator/static/js/2.170c7a75.chunk.js"
+  },
+  {
+    "revision": "887b6ad1d85c5ec103f7",
+    "url": "/calculator/static/js/main.01355e40.chunk.js"
+  },
+  {
+    "revision": "4ed1cf36ed9e5a064592",
+    "url": "/calculator/static/js/runtime-main.b8dd65a4.js"
+  }
+]);
